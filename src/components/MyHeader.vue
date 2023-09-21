ba105f17b3e38510962fde3c7b00b7b53b6bdedb
L-Vinicius <viniciusjosepereiralacerda@hotmail.com>
2023-09-21 15:03:50 -0300

    sa

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,27 @@
+<template>
+    <header class="header">
+        HEARDER
+    </header>
+</template>
+
+
+
+<script>
+    export default {
+      name: 'MyHeader'
+          
+    }
+
+</script>
+
+
+
+<style>
+
+    .header {
+        background: #000;
+        color: #fff;
+        text-decoration: line-through;
+    }
+
+</style>
